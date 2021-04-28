1e59f5028dc446a131d777b28e35681be7ef20d4
yamuben <yamubbenjamin@gmail.com>
2021-04-28 22:07:34 +0200

    windows buckup

--- server/routes/BlogRoute.js
+++ server/routes/BlogRoute.js
@@ -7,10 +7,13 @@ import Validator from '../middleware/validator';
 const blogRouter = express.Router();
 
 blogRouter.post('/create',verifyAuth, BlogController.createBlog);
+blogRouter.get('/getallfromapi',BlogController.getAllBlogsFromAPI);
 blogRouter.get('/all',BlogController.getAllBlogs);
 blogRouter.get('/:id',verifyAuth, BlogController.getOneBlog)
 blogRouter.delete('/:id',verifyAuth,Validator.verifyAccess,BlogController.deleteOneBlog)
 blogRouter.patch('/:id',verifyAuth,Validator.verifyAccess,BlogController.updateOneBlog)
+
+
 
 export default blogRouter;
 
